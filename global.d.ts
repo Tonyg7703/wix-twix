b277c6153055a5789dd5849e3022c37d0ddda191
Ahmed Aziz <tonyg7703@gmail.com>
2025-01-21 02:14:12 -0800

    dev: install & setup `dotenv` & `dotenv-cli`

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,16 @@
-export namespace NodeJS {
-  export type ProcessEnv = {
-    WIX_SITE_ID: string;
-    WIX_ACCOUNT_ID: string;
-    WIX_API_KEY: string;
-    WIX_CLIENT_ID: string;
-  };
+type WixEnvMap = {
+  WIX_SITE_ID: 'WIX_SITE_ID';
+  WIX_ACCOUNT_ID: 'WIX_ACCOUNT_ID';
+  WIX_API_KEY: 'WIX_API_KEY';
+  WIX_CLIENT_ID: 'WIX_CLIENT_ID';
+};
+
+type WixEnv = EnumToObject<WixEnvMap>;
+type EnumToObject<T extends Record<string, string>> = {
+  [K in keyof T]: string;
+};
+
+declare namespace NodeJS {
+  /* eslint-disable @typescript-eslint/no-empty-object-type */
+  export interface ProcessEnv extends WixEnv {}
 }
