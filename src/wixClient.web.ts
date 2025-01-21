185e331db02449e7e7e83a5c188bb506ee88795f
Ahmed Aziz <tonyg7703@gmail.com>
2025-01-21 02:42:21 -0800

    feat(core): add `WixWebClient` class

---
+++ src/wixClient.web.ts
@@ -0,0 +1,29 @@
+import {
+  OAuthStrategy,
+  type Descriptors,
+  type IOAuthStrategy,
+  type Tokens,
+} from '@wix/sdk';
+import { WixClient } from './wixClient';
+import { WebClientEnv } from './wixClient.types';
+
+export abstract class WixWebClient<
+  Modules extends Descriptors,
+> extends WixClient<Modules, IOAuthStrategy, WebClientEnv> {
+  constructor(modules?: Modules) {
+    super({ modules });
+  }
+
+  protected getEnvVariables(): WebClientEnv {
+    const clientId = this.getEnvVariable('clientId');
+    return { clientId };
+  }
+
+  protected getAuthStrategy(tokens?: Tokens): IOAuthStrategy {
+    const { clientId } = this.getEnvVariables();
+    return OAuthStrategy({
+      clientId,
+      tokens,
+    });
+  }
+}
