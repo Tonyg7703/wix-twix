15185bb4219709d388f55ca25db8607239349b3b
Ahmed Aziz <tonyg7703@gmail.com>
2025-01-21 06:51:21 -0800

    build: refactor and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,49 +1,58 @@
 import { defineConfig } from 'vitest/config';
+import fs from 'fs';
+import path from 'path';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'wix-twix',
-      fileName: (format) => `${format}/index.js`,
+const currentDir = path.resolve(process.cwd(), __dirname);
+const buildDir = 'dist';
+const esmDir = `${buildDir}/es`;
+const cjsDir = `${buildDir}/cjs`;
+
+function resetBuildDir(name: string) {
+  const buildPath = path.resolve(currentDir, name);
+  if (fs.existsSync(buildPath)) {
+    fs.rmSync(buildPath, { force: true, recursive: true });
+    fs.mkdirSync(buildPath);
+  }
+}
+
+function rollupOption(format: 'esm' | 'cjs') {
+  const dir = format === 'esm' ? esmDir : cjsDir;
+  const preserveModules = true;
+  const chunkFileNames = 'chunks/[name].js';
+  const entryFileNames = ({ name, moduleIds }) => {
+    const { ext } = path.parse(moduleIds[0]);
+    if (ext === '.ts') return `${name}.js`;
+    return `${name}${ext}`;
+  };
+
+  return { dir, format, entryFileNames, chunkFileNames, preserveModules };
+}
+
+export default defineConfig(({ command }) => {
+  if (command === 'build') {
+    resetBuildDir(buildDir);
+  }
+
+  return {
+    build: {
+      lib: {
+        name: 'wix-twix',
+        entry: 'src/index.ts',
+        fileName: (format) => `${format}/index.js`,
+      },
+      rollupOptions: {
+        external: ['@wix/sdk'],
+        output: [rollupOption('esm'), rollupOption('cjs')],
+      },
+      sourcemap: true,
+      minify: 'esbuild',
     },
-    rollupOptions: {
-      external: [],
-      output: [
-        {
-          dir: 'dist/es',
-          format: 'esm',
-          entryFileNames: '[name].js',
-          chunkFileNames: 'chunks/[name].js',
-          preserveModules: true,
-        },
-        {
-          dir: 'dist/cjs',
-          format: 'cjs',
-          entryFileNames: '[name].js',
-          chunkFileNames: 'chunks/[name].js',
-          preserveModules: true,
-        },
-      ],
-    },
-    sourcemap: true,
-    minify: 'esbuild',
-  },
-  test: {
-    globals: true,
-    environment: 'node',
-    coverage: {
-      reporter: ['text', 'json', 'html'], // Coverage reporters
-      all: true, // Include all files for coverage (not just the tested ones)
-      exclude: ['node_modules', 'dist'], // Exclude certain files/folders
-    },
-    include: ['tests/**/*.test.ts'], // Specify test file patterns
-    exclude: ['node_modules', 'dist'], // Exclude folders
-  },
-  plugins: [
-    dts({
-      outDir: ['dist/es', 'dist/cjs'],
-    }),
-  ],
+    plugins: [
+      dts({
+        outDir: [esmDir, cjsDir],
+        exclude: ['tests', 'node_modules', buildDir],
+      }),
+    ],
+  };
 });
